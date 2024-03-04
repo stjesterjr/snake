53783717ea243edfe55609c88ea434c899bdca59
stJseterJr <st.jesssssco@gmail.com>
2024-03-04 15:09:27 +0300

    print clear

--- snake.cpp
+++ snake.cpp
@@ -4,24 +4,32 @@
 #include <iostream>
 #include <conio.h>
 
-void clear() {
+
+//Stage
+const int stage_width = 100;
+const int stage_height = 25;
+const int stage_length = stage_width * stage_height;
+char stage_map[stage_length];
+
+
+
+//Prind screen=====================================================================
+void printScreen() {
+    for (int x = 0; x < stage_width; x++) {
+        for (int y = 0; y < stage_height; y++) {
+            std::cout << stage_map[x + y * stage_width];
+        }
+        std::cout << std::endl;
+    }
+}
+//Clear screen=====================================================================
+void clearScreen() {
     std::cout << "\033c" << std::endl;
 }
 
 int main()
 {
-
-    clear();
+    printScreen();
+    clearScreen();
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
